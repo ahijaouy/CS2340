da249b09b2805d488ec241f06bcbb7673277cbe3
Oberon <ahijaouy@gatech.edu>
2017-02-10 15:39:13 -0500

    initial additions of login

--- ThirstyGoat/app/src/main/java/edu/cs2340/thirstygoat/LoginActivity.java
+++ ThirstyGoat/app/src/main/java/edu/cs2340/thirstygoat/LoginActivity.java
@@ -187,17 +187,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            //mAuthTask.execute((Void) null);
+            //AsyncTask<Void, Void, Boolean> execute = mAuthTask.execute();
         }
     }
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.equals("user");
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.equals("pass");
     }
 
     /**
---
+++ ThirstyGoat/app/src/main/java/edu/cs2340/thirstygoat/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.cs2340.thirstygoat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onAddPressed(View view) {
+    }
+}
