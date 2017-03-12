663f03db463e844f6d0d8a01cc59a72e80b42b20
Walker Powell <walkerhpowell@gmail.com>
2017-03-12 13:24:52 -0400

    Overrode equals and hashCode for Location so that the hashmap that uses Locations as keys would function properly.

--- ThirstyGoat/app/src/main/java/com/goat/thirsty_goat/models/Location.java
+++ ThirstyGoat/app/src/main/java/com/goat/thirsty_goat/models/Location.java
@@ -29,4 +29,27 @@ public class Location {
     public double getLongitude() {
         return mLongitute;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        if (Double.compare(location.mLatitude, mLatitude) != 0) return false;
+        return Double.compare(location.mLongitute, mLongitute) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(mLatitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(mLongitute);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
